8a4966dd9f92745dbe2b6b70c4d16d7415c4826b
Masa Abushamleh <36239840+nerdingitout@users.noreply.github.com>
2020-11-19 14:33:09 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World Again - by Deepak'
+    return 'Hello, World Again'
 
 @app.route('/version')
 def get_version():
     return 'App version (using generic webhook): <b>1.0</b>'
-@app.route('/test')
-def get_test():
-    return 'You are accessing /test endpoint'
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
