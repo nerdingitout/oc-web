55c3245477c90a8b8039da63e052fd2d4bdc83dd
Masa Abushamleh <36239840+nerdingitout@users.noreply.github.com>
2020-11-19 11:24:06 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ def hello_world():
 
 @app.route('/version')
 def get_version():
-    return 'App version (using generic webhook): <b>2.0</b>'
-
-@app.route('/test')
-def get_test():
-    return 'You are accessing /test endpoint'
+    return 'App version (using generic webhook): <b>1.0</b>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
